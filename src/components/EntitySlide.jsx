dc1f4d66a668eae1962c9dba02967edc3768e149
Brian <brianezequielv2004@gmail.com>
2024-10-03 18:36:00 -0300

    Terminados los componentes de Top Items

--- src/components/EntitySlide.jsx
+++ src/components/EntitySlide.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
 export default function EntitySlide({ artistName, artistImg, albumName, albumImg }) {
+    const handleName = (Ename) => {
+        if (Ename.length > 20) {
+            return Ename.substring(0, 12) + '...';
+        }
+        return Ename;
+    };
 
     return (
         <div className="border-2 border-black h-[100%]  bg-cover bg-center flex flex-col justify-end" style={{ backgroundImage: `url(${artistImg || albumImg})` }}>
-            <p className="text-white bg-black bg-opacity-50 p-1">{artistName || albumName}</p>
+            <p className="text-white bg-black bg-opacity-50 p-1">{handleName(artistName || albumName)}</p>
             {/* <p>{props.plays} reproducciones</p> */}
         </div>
     )
--- src/components/MostListenedEntity.jsx
+++ src/components/MostListenedEntity.jsx
@@ -1,16 +1,46 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import EntitySlide from "./EntitySlide";
 
 export default function MostListenedEntity() {
     const [topArtists, setTopArtists] = useState([]);
     const [topSongs, setTopSongs] = useState([]);
     const [topAlbums, setTopAlbums] = useState([]);
+    const [selectedOptionAlbums, setSelectedOptionAlbums] = useState('Last Month');
+    const [selectedOptionArtists, setSelectedOptionArtists] = useState('Last Month');
+    const [fetchTopSongsEndpoint, setFetchTopSongsEndpoint] = useState('https://api.spotify.com/v1/me/top/tracks?limit=10&time_range=short_term');
+    const [fetchTopArtistsEndpoint, setFetchTopArtistsEndpoint] = useState('https://api.spotify.com/v1/me/top/artists?limit=10&time_range=short_term');
+    const [isFetching, setIsFetching] = useState(false);
+    const [loadingArtists, setLoadingArtists] = useState(false);
+
+    const handleOptionChangeAlbums = useCallback((event) => {
+        setSelectedOptionAlbums(event.target.value);
+        let timeRange = 'short_term';
+        if (event.target.value === "Last 6 months") {
+            timeRange = 'medium_term';
+        } else if (event.target.value === "Last year") {
+            timeRange = 'long_term';
+        }
+        setFetchTopSongsEndpoint(`https://api.spotify.com/v1/me/top/tracks?limit=10&time_range=${timeRange}`);
+        setTopSongs([]); // Reset topSongs when the endpoint changes
+    }, []);
+
+    const handleOptionChangeArtists = useCallback((event) => {
+        setSelectedOptionArtists(event.target.value);
+        let timeRange = 'short_term';
+        if (event.target.value === "Last 6 months") {
+            timeRange = 'medium_term';
+        } else if (event.target.value === "Last year") {
+            timeRange = 'long_term';
+        }
+        setFetchTopArtistsEndpoint(`https://api.spotify.com/v1/me/top/artists?limit=10&time_range=${timeRange}`);
+        setLoadingArtists(true); // Set loading state for artists
+    }, []);
 
     useEffect(() => {
         const token = window.localStorage.getItem("token");
 
-        if (token) {
+        if (token && !isFetching) {
+            setIsFetching(true);
             let options = {
                 method: "GET",
                 headers: {
@@ -20,69 +50,35 @@ export default function MostListenedEntity() {
 
             const getTopArtists = async () => {
                 try {
-                    const response = await fetch(`https://api.spotify.com/v1/me/top/artists?limit=20&time_range=short_term`, options);
+                    const response = await fetch(fetchTopArtistsEndpoint, options);
                     const data = await response.json();
-                    // console.log(data.items);
                     setTopArtists(data.items);
+                    setLoadingArtists(false); // Reset loading state for artists
                 } catch (error) {
                     console.error("ERROR: ", error);
+                    setLoadingArtists(false); // Reset loading state for artists in case of error
                 }
             };
 
-            let fetchTopSongsEndpoint = 'https://api.spotify.com/v1/me/top/tracks?limit=50';
-            let timeRange = 'short_term';
-            let isNextNull = false;
-            // let contador = 0;
             const getTopSongs = async () => {
                 let accumulatedSongs = [];
+                let nextEndpoint = fetchTopSongsEndpoint;
+                let isNextNull = false;
                 while (!isNextNull) {
                     try {
-                        const response = await fetch(`${fetchTopSongsEndpoint}&${timeRange}`, options);
+                        const response = await fetch(nextEndpoint, options);
                         const data = await response.json();
 
                         accumulatedSongs = [...accumulatedSongs, ...data.items];
-                        if (accumulatedSongs.length >= 100) {
+                        if (accumulatedSongs.length >= 100 || !data.next) {
                             isNextNull = true;
                             accumulatedSongs = accumulatedSongs.slice(0, 100); // Ensure only 100 songs are kept
+                        } else {
+                            nextEndpoint = data.next;
                         }
-                        // console.log(topSongs);
-                        // console.log(data);
-                        // topSongs = [...topSongs, ...data.items];
-                        // setTopSongs(prevState => prevState += data.items);
-                        setTopSongs(prevTopSongs => {
-                            const newTopSongs = [...prevTopSongs, ...data.items];
-                            if (newTopSongs.length >= 100) {
-                                isNextNull = true;
-                                return newTopSongs.slice(0, 100); // Ensure only 100 songs are kept
-                            }
-                            return newTopSongs;
-                        });
-                        // setTopSongs(prevTopSongs => {
-                        //     const newTopSongs = [...prevTopSongs, ...data.items];
-                        //     if (newTopSongs.length >= 100) {
-                        //         isNextNull = true;
-                        //         return newTopSongs.slice(0, 100);
-                        //     }
-                        //     return newTopSongs;
-                        // });
-                        // console.log(topSongs.length);
-                        // console.log("TOP SONGS LENGTH: ", topSongs.length);
-                        // console.log("VUELTA ", contador, ". NEXT: ", data.next);
-
-                        // console.log(topSongs.length);
-                        if (topSongs.length >= 100) {
-                            isNextNull = true;
-                        }
-                        else {
-                            if (data.next != null) {
-                                fetchTopSongsEndpoint = data.next;
-                            } else {
-                                isNextNull = true;
-                            }
-                        }
-                        // contador ++;
                     } catch (error) {
                         console.error("ERROR: ", error);
+                        isNextNull = true;
                     }
                 }
                 setTopSongs(accumulatedSongs);
@@ -91,11 +87,7 @@ export default function MostListenedEntity() {
 
             const getTopAlbumsBasedOnTopSongs = async (songs) => {
                 let actualSongs = await songs;
-                // console.log("Top songs: ");
-                // console.log(actualSongs);
-                let albumsTitles = actualSongs.map(song => {
-                    return song.album;
-                });
+                let albumsTitles = actualSongs.map(song => song.album);
 
                 const uniqueAlbums = albumsTitles.reduce((acc, album) => {
                     if (!acc.some(item => item.id === album.id)) {
@@ -104,75 +96,57 @@ export default function MostListenedEntity() {
                     }
                     return acc;
                 }, []);
-                // console.log(albumsTitles);
-                // console.log(uniqueAlbums);
 
-                // actualSongs.map(song => {
-                //     for(let i = 0; i <= actualSongs.length; i++){
-
-                //     }
-                // });
                 for (let i = 0; i < uniqueAlbums.length; i++) {
                     for (let c = 0; c < actualSongs.length; c++) {
-                        if (actualSongs[c].album.id == uniqueAlbums[i].id) {
+                        if (actualSongs[c].album.id === uniqueAlbums[i].id) {
                             uniqueAlbums[i].amountOfSongs += 1;
-                        };
-                    };
-                };
-
-                // console.log(uniqueAlbums);
+                        }
+                    }
+                }
 
                 let orderedAlbums = uniqueAlbums.sort((a, b) => b.amountOfSongs - a.amountOfSongs);
-                // console.log(orderedAlbums);
-                //ya tengo los discos unicos y tengo las top 100 canciones, necesito ordenar los discos en base a la cantidad de canciones que pertenezcan a los discos.
-                // metodo 1) Le agrego un metodo a cada uniqueAlbum que sea amountOfSongs que lo voy a modificar con un foreach o map sobre actual songs. Una vez lo tenga voy a ordenar el array uniqueAlbums en base a la cantidad de esa propiedad, amountOfSongs.
                 return orderedAlbums;
             };
 
             const fetchAndProcessData = async () => {
-                getTopArtists();
+                await getTopArtists();
                 let songsToGetTheTopAlbums = await getTopSongs();
-                // console.log(songsToGetTheTopAlbums);
                 setTopAlbums(await getTopAlbumsBasedOnTopSongs(songsToGetTheTopAlbums));
+                setIsFetching(false);
             };
 
             fetchAndProcessData();
         }
-    }, []);
-
-    // useEffect(() => {
-    //     console.log("El valor actual de top songs: ");
-    //     console.log(topSongs);
-    // }, [topSongs]);
-
-    // useEffect(() => {
-    //     console.log('El valor actual de top albums: ');
-    //     console.log(topAlbums);
-    // }, [topAlbums])
+    }, [fetchTopSongsEndpoint, fetchTopArtistsEndpoint, isFetching]);
 
     return (
         <>
             <div className="border-2 border-black bg-violet-300 py-2 px-1">
                 <div className="flex justify-between">
                     <p>Main artists this month</p>
-                    <p className="border-2 border-black p-1">Last 30 days <i className="fa-solid fa-arrow-down"></i></p>
+                    <select value={selectedOptionArtists} onChange={handleOptionChangeArtists} className="w-[45%] bg-white border border-gray-400 hover:border-gray-500 mx-auto py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+                        <option value="Last month">Last month</option>
+                        <option value="Last 6 months">Last 6 months</option>
+                        <option value="Last year">Last year</option>
+                    </select>
                 </div>
                 <div>
-                    {topArtists.length > 0 &&
-                        <div className="flex min-h-[16rem]">
-                            <div className="w-[50%] min-h-full mt-2">
-                                <EntitySlide artistName={topArtists[0].name} artistImg={topArtists[0].images[1].url} />
+                    {(topArtists.length > 0 || loadingArtists) &&
+                        <div className="flex min-h-[16rem] mt-4">
+                            <div className="w-[50%] min-h-full">
+                                <EntitySlide artistName={topArtists[0]?.name} artistImg={topArtists[0]?.images[1]?.url} />
                             </div>
                             <div className="w-[50%] flex flex-wrap border-2 border-gray-500">
                                 {topArtists.map((artist, i) => {
-                                    // console.log("El artista: ", artist);
-                                    if (i > 0) {
-                                        if (i < 5) {
-                                            return <div className="w-[50%] h-[50%]" key={i}>
+                                    if (i > 0 && i < 5) {
+                                        return (
+                                            <div className="w-[50%] h-[50%]" key={i}>
                                                 <EntitySlide artistName={artist.name} artistImg={artist.images[1].url} />
                                             </div>
-                                        }
+                                        );
                                     }
+                                    return null;
                                 })}
                             </div>
                         </div>
@@ -182,24 +156,28 @@ export default function MostListenedEntity() {
             <div className="border-2 border-black bg-violet-300 py-2 px-1 mt-5">
                 <div className="flex justify-between">
                     <p>Main albums this month</p>
-                    <p className="border-2 border-black p-1">Last 30 days <i className="fa-solid fa-arrow-down"></i></p>
+                    <select value={selectedOptionAlbums} onChange={handleOptionChangeAlbums} className="w-[45%] bg-white border border-gray-400 hover:border-gray-500 mx-auto py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+                        <option value="Last month">Last month</option>
+                        <option value="Last 6 months">Last 6 months</option>
+                        <option value="Last year">Last year</option>
+                    </select>
                 </div>
                 <div>
                     {topAlbums.length > 0 &&
-                        <div className="flex min-h-[16rem] mt-2 bg-pink-300">
+                        <div className="flex min-h-[16rem] bg-pink-300 mt-4">
                             <div className="w-[50%] min-h-full">
-                                {/* {console.log(topAlbums[0])} */}
                                 <EntitySlide albumName={topAlbums[0].name} albumImg={topAlbums[0].images[1].url} />
                             </div>
                             <div className="w-[50%] flex flex-wrap border-2 border-gray-500">
                                 {topAlbums.map((album, i) => {
-                                    if (i > 0) {
-                                        if (i < 5) {
-                                            return <div className="w-[50%] h-[50%]" key={i}>
+                                    if (i > 0 && i < 5) {
+                                        return (
+                                            <div className="w-[50%] h-[50%]" key={i}>
                                                 <EntitySlide albumName={album.name} albumImg={album.images[1].url} />
                                             </div>
-                                        }
+                                        );
                                     }
+                                    return null;
                                 })}
                             </div>
                         </div>
@@ -207,5 +185,5 @@ export default function MostListenedEntity() {
                 </div>
             </div>
         </>
-    )
-};
+    );
+}
